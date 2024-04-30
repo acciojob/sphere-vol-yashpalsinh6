456d1f14e1f18e9405c12170844f3bdb22fd60b9
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-04-30 16:24:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+function volume_sphere() {
+    //Write your code here
+    let radius = document.getElementById("radius").value;
+   let volume =  (4/3) * Math.PI * Math.pow(radius, 3);
+	volume = volume.toFixed(4);
+     document.getElementById("volume").value = volume
+    // console.log(radius);
+
+    return false;
+     
+    
+} 
